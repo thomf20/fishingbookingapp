1dcdc5db0ac861d2d16b87c665cf563cb33765a4
thomf20 <thom.flodqvist@gmail.com>
2024-12-03 19:15:48 +0100

    reset

---
+++ pages/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <p>© 2024 Fishing Booking App. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
