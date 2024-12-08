ae69d5ac9e8a95b38805e52007193a9ed08c8458
thomf20 <thom.flodqvist@gmail.com>
2024-12-08 16:26:32 +0100

    another fix

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from "react";
 
 interface NotificationProps {
   message: string;
-  type: 'success' | 'error'; // Olika typer av meddelanden
+  type: "success" | "error";
 }
 
-const Notification: React.FC<NotificationProps> = ({ message, type }) => {
-  const styles = {
-    padding: '10px',
-    margin: '10px 0',
-    borderRadius: '5px',
-    color: 'white',
-    backgroundColor: type === 'success' ? 'green' : 'red',
-    textAlign: 'center',
+const Notification = ({ message, type }: NotificationProps) => {
+  const styles: React.CSSProperties = {
+    padding: "10px",
+    margin: "10px 0",
+    borderRadius: "5px",
+    color: type === "success" ? "green" : "red",
+    backgroundColor: type === "success" ? "#e7f9e7" : "#fde7e7",
+    textAlign: "center" as const, 
   };
 
   return <div style={styles}>{message}</div>;
